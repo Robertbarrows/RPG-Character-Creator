2d2ae6572ea307aeaa239c1e716a8881037a8627
Robert Barrows <notree90@gmail.com>
2017-02-17 17:57:04 -0500

    Initial commit

---
+++ Controller.java
@@ -0,0 +1,16 @@
+/**
+ * This program is to take in the rules set forth by the user to randomly generate character's statistics:
+ * Class, Gender, Strength/Magic/Dexterity points, extra abilities or trades.  This will also save
+ * it to a file which is readable/printable for a dungeon master
+ * @author Robert Barrows
+ * @version 2/11/2017
+ */
+public class Controller {
+;
+	public static void main(String[] args) {
+		View frm = new View("Character Selection");
+
+	    frm.setSize( 500, 500 );     
+	    frm.setVisible( true );  
+	}
+}
