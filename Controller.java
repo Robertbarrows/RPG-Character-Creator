149e0699ea29dfad8e12db93fcfc3944dc8b5fe6
Robert Barrows <notree90@gmail.com>
2017-02-19 10:47:33 -0500

    Update Controller.java
    added constructor for controller class, removed the references to the setting of the JFrame in main method and added other classes into the main method to begin working on creating the character and the character stats.

--- Controller.java
+++ Controller.java
@@ -3,13 +3,23 @@
  * Class, Gender, Strength/Magic/Dexterity points, extra abilities or trades.  This will also save
  * it to a file which is readable/printable for a dungeon master
  * @author Robert Barrows
- * @version 2/11/2017
+ * @version 2/19/2017
  */
 public class Controller {
 	public static void main(String[] args) {
-		View frm = new View("Character Selection");
-
-	    frm.setSize( 500, 500 );     
-	    frm.setVisible( true );  
+		CharacterCreation frm = new CharacterCreation("Character Selection");
+		Controller control = new Controller();
+		Character createdCharacter = new Character(1);
+		Stats stats = new Stats();
+	    
+	   // createdCharacter.setCharacterClass();
+	   // createdCharacter.setGender();
+	   // createdCharacter.setRace();
+	   // stats.setStrength();
+	   // stats.setMagic();
+	   // stats.setDexterity();
+	}
+	public Controller(){
+		
 	}
 }
